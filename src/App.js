23f15e3d64abd03da2ce044b4cba4f360b691e43
maxinth <tunde.j.adeleye@gmail.com>
2021-01-14 21:29:41 +0100

    added HeroSection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./heroSection.css";
+import heroImg from "../../assets/wave-purple.png";
+const HeroSection = () => {
+  return (
+    <section className="heroSection">
+      <section className="heroSection__main">
+        <h3 className="heroSection__title">Welcome to Careersome</h3>
+        <p className="heroSection__text">
+          Careersome exist to help young Nigerians make smart, well-informed
+          decisions about their careers and enter into the labour market
+        </p>
+        <div className="heroSection__btnContainer">
+          <button className="heroSection__btn">internships</button>
+          <button className="heroSection__btn">entry level jobs</button>
+        </div>
+      </section>
+      <div className="heroSection__imgContainer">
+        <img src={heroImg} alt="hero img" className="heroSection__img" />
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
