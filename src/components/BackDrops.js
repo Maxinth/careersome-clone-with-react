6fc8ca25b6558cbfd06b6af1aed0957bea1df9a6
maxinth <tunde.j.adeleye@gmail.com>
2021-01-22 08:23:32 +0100

    added transitions to navSideBar and modal backdrops

--- src/components/BackDrops.js
+++ src/components/BackDrops.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { useGlobalContext } from "./context";
+import { motion } from "framer-motion";
+import { useVariants } from "./useVariants";
 import ModalSocialDropDown from "../components/skillsPageComponent/SkillsDefinition/ModalSocialDropDown";
 
 const BackDrops = () => {
   const { showModal, sideBarOpen, toggleSideBar } = useGlobalContext();
+  const { pageVariant, variantProps } = useVariants();
   return (
     <>
       {showModal && <ModalSocialDropDown />}
       {sideBarOpen && (
-        <div className="nav__sideBarBackdrop" onClick={toggleSideBar}></div>
+        <motion.div
+          variants={pageVariant(0.5, 0)}
+          {...variantProps}
+          className="nav__sideBarBackdrop"
+          onClick={toggleSideBar}
+        ></motion.div>
       )}
     </>
   );
--- src/components/skillsPageComponent/SkillsDefinition/ModalSocialDropDown.js
+++ src/components/skillsPageComponent/SkillsDefinition/ModalSocialDropDown.js
@@ -3,15 +3,27 @@ import SkillsSocialDropDown from "./SkillsSocialDropDown";
 import socialDropData from "./modalSocialDropDownData";
 import "./modalSocialDrop.css";
 import { useGlobalContext } from "../../context";
+import { motion } from "framer-motion";
+import { useVariants } from "../../useVariants";
 
 const ModalSocialDropDown = () => {
   const { toggleModalView } = useGlobalContext();
+  const { variantProps, pageVariant } = useVariants();
   return (
     <>
-      <section className="modalDropBox" onClick={toggleModalView}></section>
-      <div className="modalSocialDropDown">
+      <motion.div
+        className="modalSocialDropDown"
+        variants={pageVariant(0, 0)}
+        {...variantProps}
+      >
         <SkillsSocialDropDown data={socialDropData} Modal={true} />
-      </div>
+      </motion.div>
+      <motion.section
+        className="modalDropBox"
+        onClick={toggleModalView}
+        variants={pageVariant(1, 0)}
+        {...variantProps}
+      ></motion.section>
     </>
   );
 };
