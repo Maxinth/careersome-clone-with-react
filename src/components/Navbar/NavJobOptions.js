3056634fb6eb0e6ba0ec96be98308c69c8437949
maxinth <tunde.j.adeleye@gmail.com>
2021-01-26 08:31:06 +0100

    added styles to indicate the link clicked on while navigating through the navJobsOptions

--- src/components/Navbar/NavJobOptions.js
+++ src/components/Navbar/NavJobOptions.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./navJobsOption.css";
 import data from "./navJobsData";
+
 const NavJobOptions = () => {
+  const location = useLocation();
+
   return (
     <ul className="navJobOptions">
-      {data.map((item, index) => (
-        <li className="navJobOptions__listItem" key={index}>
-          <Link to={item.goTo} className="navJobOptions__link">
-            {item.linkText}
-          </Link>
-        </li>
-      ))}
+      {data.map((item, index) => {
+        const classByLocation =
+          location.pathname === item.goTo ? "activeListLink" : "";
+        return (
+          <li
+            className={`navJobOptions__listItem ${classByLocation}`}
+            key={index}
+          >
+            <Link to={item.goTo} className="navJobOptions__link">
+              {item.linkText}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
